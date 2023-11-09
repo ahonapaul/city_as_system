f6c2a474448ef30efc8308ff5d5a6cbab1059fd5
Ahona Paul <paulahona03@gmail.com>
2023-11-08 23:23:11 -0500

    first work sesh

---
+++ js/object.js
@@ -0,0 +1,19 @@
+let info = {
+    historyStart : "A",
+    historyText : "church in the corner of a quiet street, a haven for the block, was engulfed in flames on the night of December 20, 1997. In the early hours of the day, maintenance workers and the pastor’s family gathered around the rubble and ashes, their ears ringing from the silence. Just an hour or so ago, the fire department was here, filling the night’s quiet with the loud sirens and the shout of the firemen.",
+    rebuildStart: "",
+    rebuildText : "",
+    presentStart : "",
+    presentText : "",
+    dates : [1896, 1968, 1997, 2013, 2020, 2023],
+    peopleNames : ["Fowler and Hough", "Rev Charles Nesbitt", "Rev Owen L. Loftman", "Bryan E. Kelly"],
+    churchNames : ["Bushwick Avenue Congretational Church", "Bethesda Baptist Church", "Grace Restoration Temple"],
+    buildings : [1162, 1170],
+    streets : ["Bushwick Ave", "Cornelia Street", "Putnam Avenue"]
+}
+
+document.querySelector(".text1").innerHTML = info.dates[0];
+
+document.querySelector(".history_start").innerHTML = info.historyStart;
+
+document.querySelector(".history_text").innerHTML = info.historyText;
